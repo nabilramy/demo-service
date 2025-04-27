88a2be0f1caa5e2bb992b2fc94ac6c0a87c57f07
root <nabil.abushaban@gmail.com>
2025-04-27 18:20:56 +0300

    add empty route

--- src/routers/demo.ts
+++ src/routers/demo.ts
@@ -3,6 +3,10 @@ import { pushTrigger, objectNotification } from '../controllers/demo';
 import pubsubBodyParser from "../middlewares/pubsubBodyParser";
 const router = r.Router();
 
+router.get('/', (req, res) => {
+    res.send('Service is running!');
+  });
+
 router.post('/push-trigger', [pubsubBodyParser] , pushTrigger);
 
 router.post('/object-notification', [pubsubBodyParser] , objectNotification);
